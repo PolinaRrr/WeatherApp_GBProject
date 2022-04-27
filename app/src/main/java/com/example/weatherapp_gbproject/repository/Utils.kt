e15758cc266e04984c80acc159093be779cdffe8
PolinaRrr <poniapocalypsisa@gmail.com>
2022-04-27 14:25:49 +0300

    Les 6 ДЗ Пофикшена ошибка FATAL EXCEPTION: IntentService[]+рефакторинг кода

--- app/src/main/java/com/example/weatherapp_gbproject/repository/Utils.kt
+++ app/src/main/java/com/example/weatherapp_gbproject/repository/Utils.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp_gbproject.repository
 const val KEY_BUNDLE_WEATHER = "key"
 const val KEY_WEATHER_LOADER_YANDEX_QUERY = "X-Yandex-API-Key"
 const val YANDEX_DOMAIN = "https://api.weather.yandex.ru/"
-const val EXPERIMENTAL_DOMAIN = "http://212.86.114.27"
+const val EXPERIMENTAL_DOMAIN = "http://212.86.114.27/"
 const val YANDEX_TARIFF_VERSION = "v2/informers?"
 const val KEY_CONNECTION_SERVICE_LAT = "lat"
 const val KEY_CONNECTION_SERVICE_LON = "lon"
--- app/src/main/java/com/example/weatherapp_gbproject/view/ConnectionService.kt
+++ app/src/main/java/com/example/weatherapp_gbproject/view/ConnectionService.kt
@@ -2,23 +2,31 @@ package com.example.weatherapp_gbproject.view
 
 import android.app.IntentService
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.weatherapp_gbproject.BuildConfig
 import com.example.weatherapp_gbproject.repository.*
+import com.example.weatherapp_gbproject.viewmodel.ResponseState
 import com.google.gson.Gson
+import com.google.gson.JsonIOException
+import com.google.gson.JsonSyntaxException
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class ConnectionService(private val notification: String = "") : IntentService(notification) {
 
-//TODO Worker
-//    override fun doWork(): Result {
-//
-//    }
+/*TODO Worker
+  *  override fun doWork(): Result {
+  * }
+ */
 
+    @RequiresApi(Build.VERSION_CODES.N)
     @Deprecated(
         "Deprecated in Java",
         ReplaceWith("Log.d(\"@@@\", \"do work\")", "android.util.Log")
@@ -26,30 +34,70 @@ class ConnectionService(private val notification: String = "") : IntentService(n
     override fun onHandleIntent(intent: Intent?) {
         Log.d("@@@", "do work")
         intent?.let {
-            val lat = it.getDoubleExtra(KEY_CONNECTION_SERVICE_LAT,0.0)
-            val lon = it.getDoubleExtra(KEY_CONNECTION_SERVICE_LON,0.0)
-            Log.d("@@@", "work $lat $lon")
+            val lat = it.getDoubleExtra(KEY_CONNECTION_SERVICE_LAT, 0.0)
+            val lon = it.getDoubleExtra(KEY_CONNECTION_SERVICE_LON, 0.0)
 
+            //написать функцию аналог коннект сервер
 
-            val urlText = "$EXPERIMENTAL_DOMAIN${YANDEX_TARIFF_VERSION}lat=$lat&lon=$lon"
-            val uri = URL(urlText)
-            val urlConnection: HttpURLConnection = uri.openConnection() as HttpURLConnection
+            if ((0..10).random() < 5) {
+                connectServer(true, lat, lon)
+            } else {
+                connectServer(false, lat, lon)
+            }
+        }
+    }
+
+    private fun connectServer(isHTTP: Boolean, lat: Double, lon: Double) {
+        val urlText = if (isHTTP) {
+            "$EXPERIMENTAL_DOMAIN${YANDEX_TARIFF_VERSION}lat=$lat&lon=$lon"
+        } else {
+            "$YANDEX_DOMAIN${YANDEX_TARIFF_VERSION}lat=$lat&lon=$lon"
+        }
+
+        val uri = URL(urlText)
+        val urlConnection = if (isHTTP) {
+            uri.openConnection() as HttpURLConnection
+        } else {
+            uri.openConnection() as HttpsURLConnection
+        }
+
+        var responseCode = 200
+        try {
             urlConnection.addRequestProperty(
                 KEY_WEATHER_LOADER_YANDEX_QUERY,
                 BuildConfig.WEATHER_API_KEY
             )
-            //Log.d("@@@", "${urlConnection.responseCode} ${urlConnection.responseMessage}")
+            responseCode = urlConnection.responseCode
+
+            Log.d("@@@", "$responseCode ${urlConnection.responseMessage}")
 
             val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
             val response: String = buffer.readText()
+            try {
+                val weatherDTO: WeatherDTO = Gson().fromJson(response, WeatherDTO::class.java)
 
-            val weatherDTO: WeatherDTO = Gson().fromJson(response, WeatherDTO::class.java)
+                val msg = Intent(KEY_NOTIFICATION_BROADCAST_RECEIVER_WAVE)
+                msg.putExtra(KEY_BUNDLE_SERVICE_BROADCAST_WEATHER, weatherDTO)
+                LocalBroadcastManager.getInstance(this).sendBroadcast(msg)
 
+            } catch (e: JsonIOException) {
+                Log.d("%%% ", response)
+                ResponseState.ErrorJson(e)
 
-            val msg = Intent(KEY_NOTIFICATION_BROADCAST_RECEIVER_WAVE)
-            msg.putExtra(KEY_BUNDLE_SERVICE_BROADCAST_WEATHER, weatherDTO)
-            LocalBroadcastManager.getInstance(this).sendBroadcast(msg)
+            } catch (e: JsonSyntaxException) {
+                Log.d("%%% ", response)
+                ResponseState.ErrorJson(e)
+            }
 
+        } catch (e: IOException) {
+            if (responseCode in 400..499) {
+                ResponseState.ErrorConnectionFromClient(e)
+            } else {
+                ResponseState.ErrorConnectionFromServer(Exception())
+            }
+
+        } finally {
+            urlConnection.disconnect()
         }
     }
 }
